57f98d4faea5383b37917a466f3feb7f9ae69282
Thomas R. Koll <tomk32@tomk32.de>
2013-03-04 23:06:26 +0100

    Move interaction code from Actor to Player

--- actors/actor.lua
+++ actors/actor.lua
@@ -3,36 +3,6 @@ Actor = class("Actor")
 function Actor:initialize()
   self.dt_between_step = 1
 end
-
-function Actor:keydown(dt)
-  local movement = {x = 0, y = 0}
-  local dt_change = false
-  if self.dt_since_input > self.dt_between_step then
-    for key, m in pairs(self.inputs) do
-      if love.keyboard.isDown(key) then
-        if type(m) == 'function' then
-          if self.dt_since_input > 0.5 then
-            dt_change = true
-            m(self, key)
-          end
-        else
-          self.dt_since_input = 0
-          self.moved = true
-          movement.x = movement.x + m.x
-          movement.y = movement.y + m.y
-        end
-      end
-    end
-    if self.moved then
-      self:move(movement)
-    end
-  end
-  if dt_change then
-    self.dt_since_input = 0
-  end
-  self.dt_since_input = self.dt_since_input + dt
-end
-
 function Actor:move(offset)
   self.position.x = self.position.x + offset.x
   self.position.y = self.position.y + offset.y
@@ -44,7 +14,6 @@ end
 
 
 function Actor:update(dt)
-  self:keydown(dt)
   if not self.moved then
     return false
   end
--- actors/player.lua
+++ actors/player.lua
@@ -51,13 +51,42 @@ end
 
 function Player:update(dt)
   self.moved = false
-  Actor.update(self, dt)
+  self:keydown(dt)
 
   if not self.moved then
     return false
   end
 
-  -- things that happen to the player
-
+  self.map:fitIntoMap(self.position)
 end
+
+function Player:keydown(dt)
+  local movement = {x = 0, y = 0}
+  local dt_change = false
+  if self.dt_since_input > self.dt_between_step then
+    for key, m in pairs(self.inputs) do
+      if love.keyboard.isDown(key) then
+        if type(m) == 'function' then
+          if self.dt_since_input > 0.5 then
+            dt_change = true
+            m(self, key)
+          end
+        else
+          self.dt_since_input = 0
+          self.moved = true
+          movement.x = movement.x + m.x
+          movement.y = movement.y + m.y
+        end
+      end
+    end
+    if self.moved then
+      self:move(movement)
+    end
+  end
+  if dt_change then
+    self.dt_since_input = 0
+  end
+  self.dt_since_input = self.dt_since_input + dt
+end
+
 
