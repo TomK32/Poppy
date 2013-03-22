51c7a199932916eb03558fcd56f78064ad0b9e1f
Thomas R. Koll <tomk32@tomk32.de>
2013-03-22 13:12:15 +0100

    Slightly fix animation for tina and add front view of the tourist

--- animations.lua
+++ animations.lua
@@ -17,8 +17,8 @@ local animations = {
     walking_left = createAnimation('images/tina_walking_48.png', default_grid_size, {'loop', '4-6,1', 1.4})
   },
   chris = {
-    standing = createAnimation('images/chris_standing_48.png', default_grid_size, {'loop', '1-2,1', 0.8}),
-    walking_right = createAnimation('images/chris_walking_48.png', default_grid_size, {'loop', '1-2,1', 0.8}),
+    standing = createAnimation('images/chris_standing_48.png', default_grid_size, {'loop', '1-2,1', 1.4}),
+    walking_right = createAnimation('images/chris_walking_48.png', default_grid_size, {'loop', '1-2,1', 1.4}),
     walking_left = createAnimation('images/chris_walking_48.png', default_grid_size, {'loop', '1-2,1', 0.8})
   },
   poppy = {
@@ -28,9 +28,9 @@ local animations = {
   },
   tourist = {
     {
-      standing = createAnimation('images/tourist_1_walking_48.png', default_grid_size, {'loop', '1-2,1', 0.8}),
-      walking_right = createAnimation('images/tourist_1_walking_48.png', default_grid_size, {'loop', '1-4,1', 0.8}),
-      walking_left = createAnimation('images/tourist_1_walking_48.png', default_grid_size, {'loop', '5-8,1', 0.8}),
+      standing = createAnimation('images/tourist_1_48.png', default_grid_size, {'loop', '1-2,1', 2.8}),
+      walking_right = createAnimation('images/tourist_1_48.png', default_grid_size, {'loop', '3-6,1', 1.4}),
+      walking_left = createAnimation('images/tourist_1_48.png', default_grid_size, {'loop', '7-10,1', 0.8}),
     }
   },
   diary = createAnimation('images/diary_48.png', default_grid_size, {'loop', '1-2,1', 0.8})
